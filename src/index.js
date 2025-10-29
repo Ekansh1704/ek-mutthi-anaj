b018a67e2ca78b949e3cd1d98cf2d6a4934c36b5
Ekansh1704 <ekansh.chandak@gmail.com>
2025-10-29 19:47:05 +0530

    Updated again

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    {/* 
-      basename ensures routes work correctly both locally and 
-      when deployed to GitHub Pages (subpath under /ek-mutthi-anaj).
-    */}
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
